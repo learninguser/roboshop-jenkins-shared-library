94460ff0dd4d397b23c6f02f3484756870e7b146
Pavan Kumar Akula <akulapavankumar@outlook.com>
2023-01-30 18:05:57 +0100

    Add conditions for different package builders

--- vars/common.groovy
+++ vars/common.groovy
@@ -2,4 +2,11 @@ def compile(){
     if(app_lang == "nodejs"){
         sh "npm install"
     }
+    if(app_lang == "maven"){
+        sh "mvn package"
+    }
+    if(app_lang == "golang"){
+        sh "go get"
+        sh "go build"
+    }
 }
