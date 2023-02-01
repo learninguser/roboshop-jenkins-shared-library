a90f3bf0f6a20286bffb8b824dc1c009d1bfb8b7
Pavan Kumar Akula <akulapavankumar@outlook.com>
2023-02-01 16:46:25 +0100

    Add code for Masking sensitive information

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -21,11 +21,16 @@ def call(){
                 }
                 stage('Quality control'){
                     environment{
-                        SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.password  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                        // SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.password  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
                         SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
                     }
                     steps{
-                        sh "sonar-scanner -Dsonar.host.url=http://sonarqube.learninguser.online:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
+                        script{
+                            SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.password  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+                            wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
+                                sh "sonar-scanner -Dsonar.host.url=http://sonarqube.learninguser.online:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component}"
+                            }
+                        }
                     }
                 }
                 stage('Upload code to centralised place'){
