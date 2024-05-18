9e24b45b8a514865621727fce61e05ab7f33f8b3
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-05-18 09:00:32 +0200

    Move Sonar properties to a separate file

--- vars/common.groovy
+++ vars/common.groovy
@@ -18,7 +18,8 @@ def compile(appType){
 
 def codeQuality(){
     stage("Code Quality") {
-        sh "sonar-scanner -Dsonar.qualitygate.wait=true -Dsonar.token=sqa_fe67dd14b0a22f7437ea212c77ba7e23feb9b356 -Dsonar.host.url=http://172.31.13.135:9000 -Dsonar.projectKey=${env.COMPONENT}"
+        // values for sonar.host.url and sonar.login properties should be configured inside /opt/sonar/conf/sonar-scanner.properties file in the sonar server
+        sh "sonar-scanner"
     }
 }
 
